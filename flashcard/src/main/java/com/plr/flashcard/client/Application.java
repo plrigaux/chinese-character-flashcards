b70831026beb5270126ad9d6fb6b341281eb2cd1
plrigaux <plrigaux@gmail.com>
2011-07-22 05:05:11 +0000

    wo lei

--- flashcard/src/main/java/com/plr/flashcard/client/Application.java
+++ flashcard/src/main/java/com/plr/flashcard/client/Application.java
@@ -1,73 +1,24 @@
 package com.plr.flashcard.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.event.logical.shared.ValueChangeEvent;
-import com.google.gwt.event.logical.shared.ValueChangeHandler;
-import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.VerticalPanel;
-import com.plr.flashcard.client.view.MyCard;
-import com.plr.flashcard.client.view.dictionnary.CwCellList;
-import com.plr.flashcard.client.view.shishenme.ShiShenme;
-import com.plr.flashcard.client.view.welcome.Welcome;
+import com.plr.flashcard.client.view.main.MainPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class Application implements EntryPoint, ValueChangeHandler<String> {
+public class Application implements EntryPoint {
 
-	// final private static Logger logger =
-	// Logger.getLogger(Application.class.getName());
 
-	final VerticalPanel vp = new VerticalPanel();
-	
 	@Override
 	public void onModuleLoad() {
 
-		History.addValueChangeHandler(this);
-
-		FlowPanel sp = new FlowPanel();
-		sp.addStyleName("center");
-
-		sp.add(vp);
+		
 		RootLayoutPanel rootPanel = RootLayoutPanel.get();
 		// ScrollPanel sp = new ScrollPanel();
 
-		sp.add(vp);
-		// vp.add(new Label("Test"));
-		
-		sp.add(new ShiShenme());
-		
-		rootPanel.add(sp);
-
-		// final MyCard mc = new MyCard(dataControler);
-
-		History.newItem("");
-	}
-
-	@Override
-	public void onValueChange(ValueChangeEvent<String> event) {
-
-		String value = event.getValue();
-
-		if ("flash".equals(value)) {
-			vp.clear();
-			 MyCard mc = new MyCard();
-			vp.add(mc);
-		} else if ("dictionnary".equals(value)) {
-			vp.clear();
-			CwCellList charDictionnary = new CwCellList();
-			vp.add(charDictionnary);
-		} else if ("shiShenme".equals(value)) {
-			vp.clear();
-			ShiShenme charDictionnary = new ShiShenme();
-			vp.add(charDictionnary);
-		} else {
-			vp.clear();
-			Welcome welcome = new Welcome();
-			vp.add(welcome);
-		}
+		rootPanel.add(new MainPanel());
 
 	}
+
 }
