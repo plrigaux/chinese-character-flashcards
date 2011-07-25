5eb4603a2bda048454bbbd5501d217247f7baff1
plrigaux <plrigaux@gmail.com>
2011-07-25 21:30:09 +0000

    new celltable

--- flashcard/src/main/java/com/plr/flashcard/client/view/welcome/Welcome.java
+++ flashcard/src/main/java/com/plr/flashcard/client/view/welcome/Welcome.java
@@ -2,9 +2,7 @@ package com.plr.flashcard.client.view.welcome;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.Widget;
 
 public class Welcome extends Composite {
