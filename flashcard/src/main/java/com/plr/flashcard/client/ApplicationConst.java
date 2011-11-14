d7f65949b1e42f0a347421a5d802fb8dd5be20fa
plrigaux <plrigaux@gmail.com>
2011-11-14 13:41:56 +0000

    New

---
+++ flashcard/src/main/java/com/plr/flashcard/client/ApplicationConst.java
@@ -0,0 +1,8 @@
+package com.plr.flashcard.client;
+
+public interface ApplicationConst {
+	 String FLASH = "flash";
+	 String DICTIONNARY = "dictionnary";
+	 String SHI_SHENME = "shiShenme";
+	 String CHARARCTER = "character";
+}
