457792db3d73ae76f7e36566056d81450c279acb
plrigaux@gmail.com <plrigaux@gmail.com@2ef2a9fa-82eb-4ab8-1422-bdf5d29e1ec1>
2011-12-27 22:54:51 +0000

    test supermemo

---
+++ trunk/hanzi/client/src/test/java/com/plr/hanzi/client/supermemo/RecordSaverJava.java
@@ -0,0 +1,17 @@
+package com.plr.hanzi.client.supermemo;
+
+public class RecordSaverJava implements RecordSaver {
+
+	@Override
+	public String serializeToJson(Records recordMap) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Records deserializeFromJson(String json) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
